65a6f3156e6e7b6be45059440fac9ef241b80d13
Babatunde <babatundea15@gmail.com>
2019-11-11 20:09:53 +0100

    I created routes and defined paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-
+import {Route} from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Navigation />
+    <Route path="/" component ={Home}/>
+    <Route path= "/Navigation" component ={Navigation}/>
+    <Route path="/About" component= {About}/>
+    <Route path="/Contact" component={Contact}/>
+    {/* <Navigation /> */}
   </div>
 );
 
